f3669224939ff823a0eecacc5644afc261a05847
thomas <thomas.sunflower1@gmail.com>
2023-08-07 10:39:36 +0800

    '0807'

---
+++ react/backstage-management-demo/src/App.js
@@ -0,0 +1,10 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/login/index";
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+export default App;
